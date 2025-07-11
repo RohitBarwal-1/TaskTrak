d8f1e27936bdb0e3af2f1f418d4986ebc75fb544
Rohit Barwal <rohitbarwal75@gmail.com>
2025-07-11 16:14:34 +0530

    removed unrequired files

--- database/config.py
+++
@@ -1,4 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URI = "mongodb://tickettrak_dev:Development_123@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&authSource=TicketTrak&appName=mongosh+2.5.5"
-client = AsyncIOMotorClient(MONGO_URI)
--- database/models/users_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Users(BaseModel):
-    user_id: str
-    first_name: str
-    last_name: str
-    email: str
-    is_active: bool
-    password: str
